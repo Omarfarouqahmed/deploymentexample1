6cf815e6451673435fd08c3c69b5e216ddc9752f
omarfarouq ahmed <omarfarouq.ahmed@student.moringaschool.com>
2023-08-28 11:28:48 +0300

    testing casess

--- Development/codechallenges/week2python/main.py
+++ Development/codechallenges/week2python/main.py
@@ -15,6 +15,8 @@ customer1.add_review(res1, 4)
 customer2.add_review(res1, 5)
 customer2.add_review(res2, 3)
 customer3.add_review(res3, 5)
+customer1.add_review(res2, 4)
+customer1.add_review(res3, 3)
 
 # print("Customers:")
 # for customer in Customer.all_customers:
@@ -33,7 +35,13 @@ customer3.add_review(res3, 5)
 # for restaurant in Restaurant.all_restaurants:
 #     print(f"{restaurant.name}")
 
-print("Special List of Customers and Their Reviewed Restaurants:")
+# print("Special List of Customers and Their Reviewed Restaurants:")
+# for customer in Customer.all_customers:
+#     reviewed_restaurants = ", ".join([review.restaurant.name for review in customer.reviews])
+#     print(f"{customer.full_name()} reviewed: {reviewed_restaurants}")
+
+print("Restaurants Reviewed by Customers with Ratings:")
 for customer in Customer.all_customers:
-    reviewed_restaurants = ", ".join([review.restaurant.name for review in customer.reviews])
-    print(f"{customer.full_name()} reviewed: {reviewed_restaurants}")
+    print(f"{customer.full_name()} reviewed:")
+    for review in customer.reviews:
+        print(f"- {review.restaurant.name} with a rating of {review.rating}")
